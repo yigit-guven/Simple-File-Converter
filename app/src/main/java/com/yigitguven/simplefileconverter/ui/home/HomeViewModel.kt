a8c3790d928d78f04040941501351f77de9bb1bd
yigit-guven <yigitguven0124@gmail.com>
2025-04-16 12:31:09 +0300

    Initial commit,

---
+++ app/src/main/java/com/yigitguven/simplefileconverter/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.yigitguven.simplefileconverter.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
